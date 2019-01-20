0cc7894195560044f74fb958299c453adaae2894
rain.wen <rain.wen@outlook.com>
2019-01-20 18:00:34 +0800

    企业微信

--- wxwork_auth_oauth/controllers/main.py
+++ wxwork_auth_oauth/controllers/main.py
@@ -7,8 +7,10 @@ from ..api.CorpApi import *
 from ..api.AbstractApi import *
 from ..api.api_errcode import *
 from odoo import api, http, SUPERUSER_ID, _
+from werkzeug.utils import redirect
 from odoo.http import request
 from odoo.exceptions import AccessDenied, AccessError, UserError, ValidationError
+from odoo.addons.web.controllers.main import db_monodb, ensure_db, set_cookie_and_redirect, login_and_redirect
 
 
 class WxworkOAuthLogin(http.Controller):
@@ -27,11 +29,22 @@ class WxworkOAuthLogin(http.Controller):
             )
         except ApiException as e:
             pass
+        # return response
+        # if not response['DeviceId']:
+        #     raise UserError(_("抱歉，您非本企业员工"))
+        # else:
+        #     print(response['UserId'])
 
-        if not response['DeviceId']:
-            raise UserError(_("抱歉，您非本企业员工"))
+        UserId = response['UserId']
+        user = request.env['res.users'].sudo().search([('userid', '=', UserId)], limit=1)
+        # print(user.name)
+        if user:
+            request.uid = user.id
+            return login_and_redirect(*(request.session.db, user.login, UserId))
         else:
-            print(response['UserId'])
+            return redirect(request.httprequest.url_root + '/web/login/')
+
+
 
 
 
--- wxwork_auth_oauth/models/res_users.py
+++ wxwork_auth_oauth/models/res_users.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+from odoo.exceptions import AccessDenied
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
+
+    @api.model
+    def wxwork_auth_oauth_signin(self, params):
+        oauth_uid = params['userid']
+        # deviceId = params['DeviceId']
+        deviceId = params['DeviceId']
+        OpenId = params['OpenId']
+
+        if not OpenId:
+            raise AccessDenied("抱歉，您非本企业员工")
