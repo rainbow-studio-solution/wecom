6f5a2328abbf339a5c97b5a130142f69af75a085
rain.wen <rain.wen@outlook.com>
2019-01-25 15:52:58 +0800

    添加 企业微信重置密码模块

---
+++ wxwork_reset_password/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "企业微信-密码重置",
+    'author': "RStudio",
+    'website': "",
+    'sequence': 1,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'category': '企业微信',
+    'version': '12.0.0.1',
+    'summary': """
+        登录页面和后台用户使用企业微信扫码验证更改密码
+        """,
+    'description': """
+功能：
+====================
+
+        """,
+    'depends': [
+        'auth_signup',
+        'wxwork_auth_oauth',
+    ],
+    'data': [
+        # 'data/ir_cron_data.xml',
+        # 'views/ir_cron_views.xml',
+    ],
+    'qweb': [
+        "static/src/xml/*.xml",
+    ],
+    'bootstrap': True,  # load translations for login screen
+    'external_dependencies': {
+        'python': [
+
+        ],
+    },
+}
