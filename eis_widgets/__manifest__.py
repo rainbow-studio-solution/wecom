2c0bc5563d27dfd267adf4d03b4d947778115b04
rain.wen <rain.wen@outlook.com>
2019-02-18 17:46:20 +0800

    EIS widgets小组件

---
+++ eis_widgets/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "EIS-小组件",
+    'author': "RStudio",
+    'website': "",
+    'sequence': 1,
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'category': 'EIS',
+    'version': '12.0.0.1',
+    'summary': """
+        EIS 企业信息化系统小组件
+        """,
+    'description': """
+功能：
+====================
+
+        """,
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'qweb': [
+        "static/src/xml/*.xml",
+    ],
+    'bootstrap': True,  # load translatins for login screen
+    'external_dependencies': {
+        'python': [
+
+        ],
+    },
+}
