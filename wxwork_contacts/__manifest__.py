b304263add619f661151dea2938b384e3ee4f622
彩虹工作室 <rain.wen@outlook.com>
2019-10-16 00:37:44 +0800

    企业微信部门视图

--- wxwork_contacts/__manifest__.py
+++ wxwork_contacts/__manifest__.py
@@ -30,10 +30,10 @@
         # 'data/wxwork_data.xml',
         # 'views/ir_cron_views.xml',
         # 'wizard/wizard_wxwork_contacts_sync.xml',
-        # 'views/hr_department_view.xml',
+        'views/hr_department_view.xml',
         # 'views/hr_employee_view.xml',
         # 'views/res_users_views.xml',
-        # 'views/wxwork_contacts_views.xml',
+        'views/wxwork_contacts_views.xml',
         'views/res_config_settings_views.xml',
     ],
     'qweb': [
