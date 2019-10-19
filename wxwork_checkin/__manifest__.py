dbd9df1abf686e28bd090be874e54e767509b576
彩虹工作室 <rain.wen@outlook.com>
2019-10-20 02:29:56 +0800

    添加企业微信-打卡模块

---
+++ wxwork_checkin/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "企业微信-打卡",
+    'author': "RStudio",
+    'website': "",
+    'sequence': 1,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'category': '企业微信',
+    'version': '12.0.0.1',
+    'summary': """
+        获取企业微信打卡规则以及打卡数据
+        """,
+    'description': """
+功能：
+====================
+
+        """,
+    'depends': [
+        'hr_attendance',
+        'wxwork_base',
+    ],
+    'data': [
+
+    ],
+    'qweb': [
+        "static/src/xml/*.xml",
+    ],
+    'bootstrap': True,  # load translations for login screen
+    # 'external_dependencies': {'python': ['skimage']},
+
+}
