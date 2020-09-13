1a69dbbf00942d07d30a7b6c5cba3efe99dadf3d
彩虹工作室 <rain.wen@outlook.com>
2020-09-13 22:45:48 +0800

    暂时不要git pull

--- wxwork_auth_oauth/models/res_config_settings.py
+++ wxwork_auth_oauth/models/res_config_settings.py
@@ -9,23 +9,28 @@ from ..helper.common import *
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    auth_agentid = fields.Char('应用ID', help='授权方的网页应用ID，在具体的网页应用中查看', config_parameter='wxwork.auth_agentid')
-    auth_secret = fields.Char("应用的密钥", config_parameter='wxwork.auth_secret')
-    auth_redirect_uri = fields.Char('授权登陆链接回调链接地址', help='授权后重定向的回调链接地址，请使用urlencode对链接进行处理',
+    auth_agentid = fields.Char('Agent Id', help='The web application ID of the authorizing party, which can be viewed in the specific web application',
+                               config_parameter='wxwork.auth_agentid', )
+    auth_secret = fields.Char(
+        "Secret", config_parameter='wxwork.auth_secret', )
+    auth_redirect_uri = fields.Char('Callback link address redirected after authorization', help='Please use urlencode to process the link',
                                     config_parameter='wxwork.auth_redirect_uri')
-    qr_redirect_uri = fields.Char('扫码登陆链接回调链接地址', help='授权后重定向的回调链接地址，请使用urlencode对链接进行处理',
-                                    config_parameter='wxwork.qr_redirect_uri')
+    qr_redirect_uri = fields.Char('Scan the QR code to log in and call back the link address', help='Please use urlencode to process the link',
+                                  config_parameter='wxwork.qr_redirect_uri')
 
     def set_oauth_provider_wxwork(self):
         client_id = self.env['ir.config_parameter'].get_param('wxwork.corpid')
-        auth_redirect_uri = self.env['ir.config_parameter'].get_param('wxwork.auth_redirect_uri')
-        qr_redirect_uri = self.env['ir.config_parameter'].get_param('wxwork.qr_redirect_uri')
+        auth_redirect_uri = self.env['ir.config_parameter'].get_param(
+            'wxwork.auth_redirect_uri')
+        qr_redirect_uri = self.env['ir.config_parameter'].get_param(
+            'wxwork.qr_redirect_uri')
 
         auth_endpoint = 'https://open.weixin.qq.com/connect/oauth2/authorize'
         qr_auth_endpoint = 'https://open.work.weixin.qq.com/wwopen/sso/qrConnect'
 
         try:
-            providers = self.env['auth.oauth.provider'].sudo().search([('enabled', '=', True)])
+            providers = self.env['auth.oauth.provider'].sudo().search(
+                [('enabled', '=', True)])
         except Exception:
             providers = []
 
