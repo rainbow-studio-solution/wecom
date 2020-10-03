0ffbbfb6474bc3a20deb49d271118294fbc06ddc
彩虹工作室 <rain.wen@outlook.com>
2020-10-04 00:34:38 +0800

    .

--- wxwork_jsapi/models/__init__.py
+++ wxwork_jsapi/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import res_config_settings
 from . import ir_config_parameter
+from . import jsapi_interface
--- wxwork_jsapi/models/res_config_settings.py
+++ wxwork_jsapi/models/res_config_settings.py
@@ -31,12 +31,63 @@ class ResConfigSettings(models.TransientModel):
         config_parameter="wxwork.jsapi_debug",
         default=False,
     )
+    #  widget="many2many_tags"
+    js_api_list = fields.Char(
+        'JS API Inertface', config_parameter="wxwork.js_api_list",)
+    # js_api_list = fields.Selection(
+    #     [
+    #         ("selectEnterpriseContact", "选人接口"),
+    #         ("openUserProfile", "打开个人信息页接口"),
+    #         ("openUserProfile", "外部联系人选人接口"),
+    #         ("selectExternalContact", "外部联系人选人接口"),
+    #         ("getCurExternalContact", "获取当前外部联系人userid"),
+    #         ("getCurExternalChat", "获取当前客户群的群ID"),
+    #         ("sendChatMessage", "聊天工具栏分享消息到会话"),
+    #         ("getContext", "聊天工具栏分享消息到会话"),
+    #         ("openEnterpriseChat", "创建会话接口"),
+    #         ("openEnterpriseChat", "创建会话接口"),
+    #         ("onMenuShareAppMessage", "分享接口-获取“转发”按钮"),
+    #         ("onMenuShareWechat", "分享接口-获取“微信”按钮"),
+    #         ("onMenuShareTimeline", "分享接口-获取“分享到朋友圈”按钮"),
+    #         ("shareAppMessage", "分享接口-自定义转发到会话"),
+    #         ("shareWechatMessage", "分享接口-自定义转发到微信"),
+    #         ("shareToExternalContact", "分享接口-将H5页面通过个人群发发送给客户"),
+    #         ("shareToExternalChat", "分享接口-将H5页面通过群发助手发送给客户群"),
+    #         ("onHistoryBack", "界面操作-监听页面返回事件"),
+    #         ("hideOptionMenu", "界面操作-隐藏右上角菜单接口"),
+    #         ("showOptionMenu", "界面操作-显示右上角菜单接口"),
+    #         ("closeWindow", "界面操作-关闭当前网页窗口接口"),
+    #         ("hideMenuItems", "界面操作-批量隐藏功能按钮接口"),
+    #         ("showMenuItems", "界面操作-批量显示功能按钮接口"),
+    #         ("hideAllNonBaseMenuItem", "界面操作-隐藏所有非基础按钮接口"),
+    #         ("showAllNonBaseMenuItem", "界面操作-显示所有功能按钮接口"),
+    #         ("openDefaultBrowser", "界面操作-打开系统默认浏览器"),
+    #         ("onUserCaptureScreen", "界面操作-用户截屏事件"),
+    #         ("scanQRCode", "企业微信扫一扫"),
+    #         ("chooseInvoice", "电子发票-拉起电子发票列表"),
+
+    #         ("startRecord", "开始录音接口"),
+    #         ("stopRecord", "停止录音接口"),
+    #         ("onVoiceRecordEnd", "监听录音自动停止接口"),
+    #         ("playVoice", "播放语音接口"),
+    #         ("pauseVoice", "暂停播放接口"),
+    #         ("stopVoice", "停止播放接口"),
+    #         ("onVoicePlayEnd", "听语音播放完毕接口"),
+    #         ("uploadVoice", "上传语音接口"),
+    #         ("downloadVoice", "下载语音接口"),
+    #         ("translateVoice", "音转文字接口"),
+    #     ],
+    #     string="JS API Inertface",
+
+    #     config_parameter="wxwork.js_api_list",
+    # )
 
     ticket_interval_time = fields.Integer(
         "Pull interval time",
         config_parameter="wxwork.ticket_interval_time",
         default=1,
     )
+
     ticket_interval_type = fields.Selection(
         [("minutes", "Minutes"), ("hours", "Hours")],
         string="Pull Interval Unit",
@@ -55,7 +106,8 @@ class ResConfigSettings(models.TransientModel):
         ir_config = self.env["ir.config_parameter"].sudo()
 
         jsapi_debug = (
-            True if ir_config.get_param("wxwork.jsapi_debug") == "True" else False
+            True if ir_config.get_param(
+                "wxwork.jsapi_debug") == "True" else False
         )
 
         res.update(
@@ -98,7 +150,8 @@ class ResConfigSettings(models.TransientModel):
         if corpid == False:
             raise UserError(_("Please fill in correctly Enterprise ID."))
         elif auth_secret == False:
-            raise UserError(_("Please fill in the application 'secret' correctly."))
+            raise UserError(
+                _("Please fill in the application 'secret' correctly."))
 
         else:
             if not self.get_ticket_last_time:
@@ -132,7 +185,8 @@ class ResConfigSettings(models.TransientModel):
                 },
             )
             if self.corp_jsapi_ticket != response["ticket"]:
-                ir_config.set_param("wxwork.corp_jsapi_ticket", response["ticket"])
+                ir_config.set_param(
+                    "wxwork.corp_jsapi_ticket", response["ticket"])
             else:
                 pass
 
@@ -172,7 +226,8 @@ class ResConfigSettings(models.TransientModel):
                 },
             )
             if self.agent_jsapi_ticket != response["ticket"]:
-                ir_config.set_param("wxwork.agent_jsapi_ticket", response["ticket"])
+                ir_config.set_param(
+                    "wxwork.agent_jsapi_ticket", response["ticket"])
                 ir_config.set_param(
                     "wxwork.get_ticket_last_time", datetime.datetime.now()
                 )
@@ -229,7 +284,8 @@ class ResConfigSettings(models.TransientModel):
         if corpid == False:
             raise UserError(_("Please fill in correctly Enterprise ID."))
         elif auth_secret == False:
-            raise UserError(_("Please fill in the application 'secret' correctly."))
+            raise UserError(
+                _("Please fill in the application 'secret' correctly."))
 
         else:
             api = CorpApi(corpid, auth_secret)
