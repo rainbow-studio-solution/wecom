9bf76055f621457d5f0998873f69d1f389845d85
彩虹工作室 <rain.wen@outlook.com>
2021-01-02 14:40:27 +0800

    优化一键登录

--- wxwork_auth_oauth/static/src/js/auth.js
+++ wxwork_auth_oauth/static/src/js/auth.js
@@ -28,20 +28,27 @@ odoo.define('wxwork_auth_oauth.auth', function (require) {
             if (!isWx) {
                 return false;
             } else {
+                var dialog;
+                this.isWxworkBrowser = false;
                 if (ua.match(/WxWork/i) == "wxwork") {
                     // 检测到是企业微信内置浏览器
-                    var dialog = $(qweb.render('wxwork_auth_oauth.LoginDialog', {}));
+                    this.isWxworkBrowser = true;
+                    dialog = $(qweb.render('wxwork_auth_oauth.LoginDialog', {
+                        isWxworkBrowser: true
+                    }));
                     if (self.$el.parents("body").find("#wxwork_login_dialog").length == 0) {
                         dialog.appendTo($(document.body));
                     }
                     dialog.modal('show');
 
                     var logon_button = dialog.find("button:first");
-                    console.log(logon_button)
+
                     logon_button.click(function (event) {
                         var url = self.$el.find("i.fa-wechat").parent().attr('href');
                         window.open(url);
                     })
+                } else {
+                    this.isWxworkBrowser = false;
                 }
             }
         },
@@ -58,6 +65,19 @@ odoo.define('wxwork_auth_oauth.auth', function (require) {
                     dialog.appendTo($(document.body));
                 }
                 dialog.modal('show');
+            } else if (icon.hasClass("fa-wechat")) {
+                var ua = navigator.userAgent.toLowerCase();
+                if (ua.match(/WxWork/i) == "wxwork") {
+                    window.open(url);
+                } else {
+                    var dialog = $(qweb.render('wxwork_auth_oauth.LoginDialog', {
+                        isWxworkBrowser: self.isWxworkBrowser
+                    }));
+                    if (self.$el.parents("body").find("#wxwork_login_dialog").length == 0) {
+                        dialog.appendTo($(document.body));
+                    }
+                    dialog.modal('show');
+                }
             } else {
                 window.open(url);
             }
