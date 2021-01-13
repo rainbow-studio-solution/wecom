a6f70d6a1e8343e1c80d5c199d2a69aae4cd882a
彩虹工作室 <rain.wen@outlook.com>
2021-01-13 21:21:09 +0800

    待处理生成消息功能

--- wxwork_message_push/models/mail_template.py
+++ wxwork_message_push/models/mail_template.py
@@ -75,27 +75,14 @@ class MailTemplate(models.Model):
         email_values=None,
         notif_layout=False,
     ):
-        values = self.generate_email(
-            res_id,
-            [
-                "subject",
-                "body_html",
-                "email_from",
-                "email_to",
-                "partner_to",
-                "email_cc",
-                "reply_to",
-                "scheduled_date",
-            ],
-        )
-        # res_id 是用户id
         if self.env["res.users"].browse(res_id).notification_type == "wxwork":
+            # res_id 是用户id
             # 拦截 用户通知类型为企业微信的发送方式
-            values = self.env["wxwork.message.template"].generate_message(
+            values = self.generate_message(
                 res_id,
                 [
                     "subject",
-                    "body_html",
+                    "wxwork_body_html",
                     "email_from",
                     "email_to",
                     "partner_to",
@@ -113,3 +100,72 @@ class MailTemplate(models.Model):
             email_values=None,
             notif_layout=False,
         )
+
+    def generate_message(self, res_ids, fields):
+        self.ensure_one()
+        multi_mode = True
+        if isinstance(res_ids, int):
+            res_ids = [res_ids]
+            multi_mode = False
+
+        results = dict()
+        for lang, (template, template_res_ids) in self._classify_per_lang(
+            res_ids
+        ).items():
+            for field in fields:
+                template = template.with_context(safe=(field == "subject"))
+                generated_field_values = template._render_field(
+                    field, template_res_ids, post_process=(field == "wxwork_body_html")
+                )
+                for res_id, field_value in generated_field_values.items():
+                    results.setdefault(res_id, dict())[field] = field_value
+            # compute recipients
+            if any(field in fields for field in ["email_to", "partner_to", "email_cc"]):
+                results = template.generate_recipients(results, template_res_ids)
+            # update values for all res_ids
+            for res_id in template_res_ids:
+                values = results[res_id]
+                if values.get("wxwork_body_html"):
+                    values["body"] = tools.html_sanitize(values["wxwork_body_html"])
+                # technical settings
+                values.update(
+                    mail_server_id=template.mail_server_id.id or False,
+                    auto_delete=template.auto_delete,
+                    model=template.model,
+                    res_id=res_id or False,
+                    attachment_ids=[attach.id for attach in template.attachment_ids],
+                )
+
+            # Add report in attachments: generate once for all template_res_ids
+            if template.report_template:
+                for res_id in template_res_ids:
+                    attachments = []
+                    report_name = self._render_field("report_name", [res_id])[res_id]
+                    report = template.report_template
+                    report_service = report.report_name
+
+                    if report.report_type in ["qweb-html", "qweb-pdf"]:
+                        result, format = report._render_qweb_pdf([res_id])
+                    else:
+                        res = report._render([res_id])
+                        if not res:
+                            raise UserError(
+                                _(
+                                    "Unsupported report type %s found.",
+                                    report.report_type,
+                                )
+                            )
+                        result, format = res
+
+                    # TODO in trunk, change return format to binary to match message_post expected format
+                    result = base64.b64encode(result)
+                    if not report_name:
+                        report_name = "report." + report_service
+                    ext = "." + format
+                    if not report_name.endswith(ext):
+                        report_name += ext
+                    attachments.append((report_name, result))
+                    results[res_id]["attachments"] = attachments
+
+        return multi_mode and results or results[res_ids[0]]
+        return super(MailTemplate, self).generate_email(res_ids, fields)
