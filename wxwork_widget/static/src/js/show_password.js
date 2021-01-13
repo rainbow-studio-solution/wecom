09399fa082679a96a64164a755f0c6a4555b2d86
彩虹工作室 <rain.wen@outlook.com>
2021-01-13 14:00:28 +0800

    [FIX]修复显示密码小挂件无法设置值的问题

--- wxwork_widget/static/src/js/show_password.js
+++ wxwork_widget/static/src/js/show_password.js
@@ -1,36 +1,31 @@
 odoo.define("wxwork_widget.ShowPasswordFieldText", function (require) {
     'use strict';
 
-    // 参考addons\auth_password_policy\static\src\js\password_field.js
-
-    var InputField = require('web.basic_fields').InputField;
+    var FieldChar = require('web.basic_fields').FieldChar;
     var fieldRegistry = require('web.field_registry');
 
-    var core = require('web.core');
-    var _lt = core._lt;
-
-
-    var ShowPasswordFieldText = InputField.extend({
+    var ShowPasswordFieldText = FieldChar.extend({
         template: 'ShowPassword',
-        description: _lt("ShowPassword"),
-        className: 'o_form_field_showpassword',
-        events: {
-            'mousedown span.wxwork_show_password': '_showPassword',
-        },
+        className: 'o_field_showpassword',
         init: function (parent) {
             this._super.apply(this, arguments);
-            // this.$input = this.$el.find('input');
-
-            console.log("init", this.value);
-            // console.log("init", this.$input.val());
         },
-        start: function () {
+        _renderEdit: function () {
+            var $input = this.$el.find('input');
+            if (this.value === null || this.value === "" || this.value === false) {
+                return this._super($input.val(""));
+            } else {
+                return this._super($input.val(this.value));
+            }
+        },
+        _prepareInput: function ($input) {
+            var self = this;
             this.$input = this.$el.find('input');
-
-            console.log("start", this.value);
-            console.log("start", this.$input.val());
-
-            return this._super.apply(this, arguments);
+            var button = self.$el.find("span.wxwork_show_password");
+            button.mousedown(function (ev) {
+                self._showPassword(ev);
+            })
+            return $.when($input, this._super.apply(this, arguments));
         },
         _showPassword: function (ev) {
             $(ev.target).closest('.input-group').find('.form-control').prop("type",
@@ -40,35 +35,11 @@ odoo.define("wxwork_widget.ShowPasswordFieldText", function (require) {
             );
             $(ev.currentTarget).toggleClass('fa-eye-slash fa-eye');
         },
-        _renderEdit: function () {
-            this.$input = this.$el.find('input');
-            console.log("_renderEdit", this.value);
-            console.log("_renderEdit", this.$input.val());
-
-            // var $input = this.$el.find('input');
-            if (this.value === null || this.value === "" || this.value === false) {
-                // return this._super($input.val(""));
-                return this._super(this.$input.val(""));
-            } else {
-                // return this._super($input.val(this.value));
-                return this._super(this.$input.val(this.value));
-            }
-        },
-        _prepareInput: function ($input) {
-            var self = this;
-            return $.when($input, this._super.apply(this, arguments));
-        },
         _setValue: function () {
             var $input = this.$el.find('input');
+
             return this._super($input.val());
         },
-        _getValue: function () {
-            console.log("_getValue", this.value);
-            console.log("_getValue", this.$input.val());
-
-            return this.$input.val();
-        },
-
     });
 
     fieldRegistry.add('wxwork_password', ShowPasswordFieldText);
