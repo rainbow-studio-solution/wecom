347b992d6d2272226c0a6848def76bda7a65196e
彩虹工作室 <rain.wen@outlook.com>
2021-01-31 16:24:29 +0800

    hr模块增加auth_oauth依赖

--- wxwork_hr/__manifest__.py
+++ wxwork_hr/__manifest__.py
@@ -11,7 +11,7 @@
     "category": "Enterprise WeChat/Enterprise WeChat",
     "version": "14.0.0.1",
     # any module necessary for this one to work correctly
-    "depends": ["hr", "wxwork_base",],
+    "depends": ["hr", "auth_oauth", "wxwork_base",],
     "data": [
         "security/ir.model.access.csv",
         "data/hr_data.xml",
