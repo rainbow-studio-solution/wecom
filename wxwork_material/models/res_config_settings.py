879f708e593d025bb4a2f4529dd3650aed2a38ae
彩虹工作室 <rain.wen@outlook.com>
2021-03-08 23:57:17 +0800

    修复配置的重名日志提示

--- wxwork_material/models/res_config_settings.py
+++ wxwork_material/models/res_config_settings.py
@@ -7,10 +7,12 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     material_agentid = fields.Char(
-        "Agent Id", config_parameter="wxwork.material_agentid", default="0000000",
+        "Material Agent Id",
+        config_parameter="wxwork.material_agentid",
+        default="0000000",
     )
     material_secret = fields.Char(
-        "Secret",
+        "Material Secret",
         config_parameter="wxwork.material_secret",
         default="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     )
