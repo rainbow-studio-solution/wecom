b95074a5e58b378b2e15b104129c1579de7b5219
彩虹工作室 <rain.wen@outlook.com>
2021-09-01 12:44:22 +0800

    添加公司简称字段

--- wxwork_base/models/res_company.py
+++ wxwork_base/models/res_company.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, tools, _
+from odoo.tools.translate import translate
 
 
 class Company(models.Model):
     _inherit = "res.company"
 
+    abbreviated_name = fields.Char("Abbreviated Name", required=True, translate=True)
     is_wxwork_organization = fields.Boolean(
         "Enterprise wechat organization", default=False
     )
