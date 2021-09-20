e2c58e5924fda5cd7afa6e1f632fce61d9e8458b
彩虹工作室 <rain.wen@outlook.com>
2021-09-20 15:50:20 +0800

    添加客服模块

---
+++ wxwork_customer_service/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Enterprise Wechat Customer Service",
+    "author": "RStudio",
+    "website": "https://gitee.com/rainbowstudio/wxwork",
+    "sequence": 610,
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "category": "Enterprise WeChat/Enterprise WeChat",
+    "version": "14.0.0.1",
+    "summary": """
+        """,
+    "description": """
+
+
+        """,
+    "depends": ["wxwork_base"],
+    "external_dependencies": {"python": [],},
+    "data": [],
+    "qweb": ["static/src/xml/*.xml",],
+}
