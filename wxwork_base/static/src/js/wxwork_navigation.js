d761aae82cd4089682685385d922b14ec5886aa3
彩虹工作室 <rain.wen@outlook.com>
2021-09-20 22:15:34 +0800

    待处理配置页面中的锚点跳转

--- wxwork_base/static/src/js/wxwork_navigation.js
+++ wxwork_base/static/src/js/wxwork_navigation.js
@@ -1,11 +1,11 @@
 odoo.define('wxwork_base.Anchor', function (require) {
     'use strict';
 
+    var ActionManager = require('web.ActionManager');
     var Widget = require('web.Widget');
     var widget_registry = require('web.widget_registry');
-    var WXWorkSettingsAnchor = Widget.extend({
-        // selector: 'div.app_settings_block',
-        template: 'res_config_wxwork_anchor',
+    var WXWorkSettingsNavigationMenu = Widget.extend({
+        template: 'res_config_wxwork_navigation_menu',
         events: {
             'click a.wxwork_jump_anchor': '_jump_anchor',
         },
@@ -13,7 +13,6 @@ odoo.define('wxwork_base.Anchor', function (require) {
             this._super.apply(this, arguments);
         },
         start: function () {
-            console.log(this.$el);
             var self = this;
             return this._super.apply(this, arguments);
         },
@@ -22,13 +21,27 @@ odoo.define('wxwork_base.Anchor', function (require) {
             var self = this;
             var anchor = $(ev.target).attr('href');
 
-            if ($(ev.target).prop("tagName") == "H5") {
+            if ($(ev.target).prop("tagName") != "A") {
                 anchor = $(ev.target).parent().attr('href');
-
             }
-            console.log("点击", anchor);
+
+            var settingsEl = this.$el.parents(".settings");
+            var wxworkSettingsEl = this.$el.parents(".app_settings_block");
+            var navEl = this.$el;
+
+            // var $settings = this.$el.parents(".app_settings_block");
+            console.log($(anchor).offset().top, navEl.outerHeight());
+            console.log($(anchor).offset().top - navEl.outerHeight());
+
+            if (wxworkSettingsEl.height() > settingsEl.height()) {
+                // settingsEl.animate({
+                //     scrollTop: $(anchor).offset().top - navEl.outerHeight(),
+                // }, 1000)
+                settingsEl.scrollTop($(anchor).offset().top - navEl.outerHeight());
+            }
+
         }
     });
-    widget_registry.add('res_config_wxwork_anchor', WXWorkSettingsAnchor);
-    return WXWorkSettingsAnchor;
+    widget_registry.add('res_config_wxwork_navigation_menu', WXWorkSettingsNavigationMenu);
+    return WXWorkSettingsNavigationMenu;
 });
