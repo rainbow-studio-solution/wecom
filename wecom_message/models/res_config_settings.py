e5937214cf98f561ce8df3cd0ebb38eaf4ca8d2d
彩虹工作室 <rain.wen@outlook.com>
2021-12-14 17:26:52 +0800

    重构企微消息

--- wecom_message/models/res_config_settings.py
+++ wecom_message/models/res_config_settings.py
@@ -18,46 +18,53 @@ class ResConfigSettings(models.TransientModel):
         default=lambda self: self.env.company,
     )
 
-    wecom_message_logo = fields.Binary(
-        related="company_id.wecom_message_logo", readonly=False
+    # 消息
+    message_app_id = fields.Many2one(
+        related="company_id.message_app_id", readonly=False
     )
-    wecom_message_logo_web = fields.Binary(
-        related="company_id.wecom_message_logo_web", readonly=False
-    )
+    message_agentid = fields.Integer(related="message_app_id.agentid", readonly=False)
+    message_secret = fields.Char(related="message_app_id.secret", readonly=False)
+    message_access_token = fields.Char(related="message_app_id.access_token")
+    # wecom_message_logo = fields.Binary(
+    #     related="company_id.wecom_message_logo", readonly=False
+    # )
+    # wecom_message_logo_web = fields.Binary(
+    #     related="company_id.wecom_message_logo_web", readonly=False
+    # )
 
-    message_agentid = fields.Char(related="company_id.message_agentid", readonly=False)
+    # message_agentid = fields.Char(related="company_id.message_agentid", readonly=False)
 
-    message_secret = fields.Char(related="company_id.message_secret", readonly=False)
+    # message_secret = fields.Char(related="company_id.message_secret", readonly=False)
 
-    module_gamification = fields.Boolean(readonly=False)
+    # module_gamification = fields.Boolean(readonly=False)
 
     # module_wecom_hr_gamification_message = fields.Boolean(
     #     related="company_id.module_wecom_hr_gamification_message", readonly=False
     # )
 
-    module_digest = fields.Boolean("KPI Digests")
+    # module_digest = fields.Boolean("KPI Digests")
     # module_wxowrk_digest_message = fields.Boolean(
     #     "Send KPI Digests periodically via WeCom",
     # )
 
-    module_stock = fields.Boolean()
+    # module_stock = fields.Boolean()
     # module_wecom_stock_message = fields.Boolean(
     #     related="company_id.module_wecom_stock_message", readonly=False
     # )
 
-    module_purchase = fields.Boolean()
+    # module_purchase = fields.Boolean()
     # module_wecom_purchase_message = fields.Boolean(
     #     "Send Purchase message via WeCom",
     # )
 
-    @api.onchange("wecom_message_logo")
-    def _onchange_wecom_message_logo(self):
-        if self.wecom_message_logo:
-            image = tools.base64_to_image(self.wecom_message_logo)
-            w, h = image.size
-            if w == h:
-                self.wecom_message_logo_web = tools.image_process(
-                    self.wecom_message_logo, size=(180, 180)
-                )
-            else:
-                raise UserError(_("Please upload a picture of the square!"))
+    # @api.onchange("wecom_message_logo")
+    # def _onchange_wecom_message_logo(self):
+    #     if self.wecom_message_logo:
+    #         image = tools.base64_to_image(self.wecom_message_logo)
+    #         w, h = image.size
+    #         if w == h:
+    #             self.wecom_message_logo_web = tools.image_process(
+    #                 self.wecom_message_logo, size=(180, 180)
+    #             )
+    #         else:
+    #             raise UserError(_("Please upload a picture of the square!"))
