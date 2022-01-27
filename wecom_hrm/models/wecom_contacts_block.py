b8e674f731b9794573ab4b59fe3edc55b2aaa6bd
彩虹工作室 <rain.wen@outlook.com>
2022-01-27 11:58:32 +0800

    修复通讯簿Block 获取名称功能

--- wecom_hrm/models/wecom_contacts_block.py
+++ wecom_hrm/models/wecom_contacts_block.py
@@ -4,6 +4,9 @@ from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
 from odoo.addons.wecom_api.api.wecom_abstract_api import ApiException
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class WxworkContactsBlock(models.Model):
@@ -11,7 +14,7 @@ class WxworkContactsBlock(models.Model):
     _description = "Wecom contacts synchronization block list"
 
     name = fields.Char(
-        string="Name", readonly=True,
+        string="Name", readonly=True, copy=False, compute="_compute_name", store=True,
     )  # required=True,readonly=True, store=True
     company_id = fields.Many2one(
         "res.company",
@@ -23,59 +26,26 @@ class WxworkContactsBlock(models.Model):
 
     wecom_userid = fields.Char(string="WeCom user Id", required=True)
 
-    def get_name(self):
-        if self.company_id is None or self.wecom_userid == False:
-            return
-        try:
-            wxapi = self.env["wecom.service_api"].InitServiceApi(
-                self.company_id, "contacts_secret", "contacts"
-            )
-            response = wxapi.httpCall(
-                self.env["wecom.service_api_list"].get_server_api_call("USER_GET"),
-                {"userid": self.wecom_userid},
-            )
-
-            params = {}
-            if "errcode" in str(response):
-                if response["errcode"] == 0:
-                    self.name = response["name"]
-
-                    params = {
-                        "title": _("Success"),
-                        "type": "success",
-                        "className": "bg-success",
-                        "message": _("Get user name successfully"),
-                        "sticky": False,  # 延时关闭
-                        "next": {},
-                    }
-
-            else:
-                self.name = ""
-                params = {
-                    "title": _("Failed"),
-                    "type": "danger",
-                    "className": "bg-danger",
-                    "message": _("Failed to read WeCom members! Error details: %s .")
-                    % (str(response)),
-                    "sticky": True,  # 不会延时关闭，需要手动关闭
-                    "next": {},
-                }
-                # self.write({"name": ""})
-
-            action = {
-                "type": "ir.actions.client",
-                "tag": "display_notification",
-                "params": {
-                    "title": params["title"],
-                    "type": params["type"],
-                    "className": params["className"],
-                    "message": params["message"],
-                    "sticky": params["sticky"],
-                    "next": {"type": "ir.actions.client", "tag": "reload",},  # 刷新窗体
-                },
-            }
-            return action
-        except ApiException as ex:
-          return self.env["wecomapi.tools.action"].ApiExceptionDialog(
-                    ex, raise_exception=True
-                )
+    @api.depends("company_id", "wecom_userid")
+    def _compute_name(self):
+        for block in self:
+            if block.company_id and block.wecom_userid:
+                company = block.company_id
+                name = ""
+                try:
+                    wxapi = self.env["wecom.service_api"].InitServiceApi(
+                        company.corpid, company.contacts_app_id.secret
+                    )
+                    response = wxapi.httpCall(
+                        self.env["wecom.service_api_list"].get_server_api_call(
+                            "USER_GET"
+                        ),
+                        {"userid": block.wecom_userid},
+                    )
+                    name = response["name"]
+                except ApiException as ex:
+                    return self.env["wecomapi.tools.action"].ApiExceptionDialog(
+                        ex, raise_exception=True
+                    )
+                finally:
+                    block.name = name
