f1f79e3aea8239a686ba736cead6731ffae961cd
彩虹工作室 <rain.wen@outlook.com>
2022-07-08 17:14:29 +0800

    在设置页面显示”企微审批“安装功能

--- wecom_base/static/src/js/wecom_settings_navigation.js
+++ wecom_base/static/src/js/wecom_settings_navigation.js
@@ -7,7 +7,7 @@ odoo.define('wecom_base.WecomSettingsNavigationMenu', function (require) {
     var WecomSettingsNavigationMenu = Widget.extend({
         template: 'res_config_wecom_navigation_menu',
         events: {
-            'click a.wecom__jump_anchor': '_jump_anchor',
+            'click a.wecom_jump_anchor': '_jump_anchor',
         },
         init: function () {
             this._super.apply(this, arguments);
