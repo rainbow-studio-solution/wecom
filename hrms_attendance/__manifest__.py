fcdf3530567db312c66a166aca61bf8b594e2b4e
彩虹工作室 <rain.wen@outlook.com>
2022-08-01 15:40:48 +0800

    添加员工报销模块，和将HR相关的设置整合在一起

--- hrms_attendance/__manifest__.py
+++ hrms_attendance/__manifest__.py
@@ -21,6 +21,7 @@
         "hr_attendance",
     ],
     "data": ["views/menu_views.xml","views/res_config_settings_views.xml"],
+    "assets": {"web.assets_qweb": ["hrms_attendance/static/src/xml/*.xml",],},
     "external_dependencies": {"python": [],},
     "license": "LGPL-3",
 }
